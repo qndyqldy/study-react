3b993d8ce2e7d41a99146bef52446752dabd7eec
ywyim <qndyqldy@gmail.com>
2024-04-07 15:04:37 +0900

    feat : day 12 미션 (배달 음식 주문 페이지 업그레이드)

---
+++ mission/mission03/src/components/OrderEditor.jsx
@@ -0,0 +1,85 @@
+import {useRef, useState} from 'react';
+
+const OrderEditor = () => {
+
+	const [order, setOrder] = useState({
+		menu: '',
+		address: '',
+		request: ''
+	});
+
+	const onChangeOrder = (e) => {
+		setOrder({
+			...order,
+			[e.target.name]: e.target.value
+		})
+	};
+
+	const inputRef = useRef();
+
+	const onSubmit = () => {
+		if(order.address === '') {
+			inputRef.current.focus();
+			return;
+		}
+		alert(`주문이 완료되었습니다. 메뉴 : ${order.menu}, 주소 : ${order.address}, 요청사항 : ${order.request}`);
+	};
+
+	return (
+		<div
+			style={{ display: "flex", flexDirection: "column", gap: 10 }}
+		>
+			<h2>배달의민족 주문</h2>
+			<div>
+				<div style={{ marginBottom: 5, fontSize: 14 }}>
+					메뉴 선택
+				</div>
+				<select
+					style={{ width: 300, padding: 5 }}
+					name="menu"
+					value={order.menu}
+					onChange={onChangeOrder}>
+					<option value={"족발"}>족발</option>
+					<option value={"떡볶이"}>떡볶이</option>
+					<option value={"아이스크림"}>아이스크림</option>
+					<option value={"샐러드"}>샐러드</option>
+				</select>
+			</div>
+			<div>
+				<div style={{ marginBottom: 5, fontSize: 14 }}>
+					배달 주소
+				</div>
+				<input
+					style={{ width: 300, padding: 5 }}
+					ref={inputRef}
+					name="address"
+					placeholder="주소) 서울특별시 xx동 .."
+					value={order.address}
+					onChange={onChangeOrder}
+				/>
+			</div>
+			<div>
+				<div style={{ marginBottom: 5, fontSize: 14 }}>
+					배달 요청사항
+				</div>
+				<textarea
+					name="request"
+					style={{ width: 300, padding: 5 }}
+					placeholder="배달 요청사항을 써 주세요..."
+					value={order.request}
+					onChange={onChangeOrder}
+				/>
+			</div>
+			<div>
+				<button
+					onClick={onSubmit}
+					style={{ width: 300, padding: 5 }}
+				>
+					주문 완료
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default OrderEditor;
