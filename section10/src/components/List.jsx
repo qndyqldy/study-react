a99dc0b918290bde89a903c04e66091f27752fd6
ywyim <qndyqldy@gmail.com>
2024-04-08 18:37:04 +0900

    feat : Todo List 업그레이드 및 최적화

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,67 @@
+import './List.css';
+import TodoItem from './TodoItem.jsx';
+import {useState, useMemo} from 'react';
+
+const List = ({todos, onUpdate, onDelete}) => {
+	const [search, setSearch] = useState('');
+	const onChange = (e) => {
+		setSearch(e.target.value);
+	}
+	const getFilteredData = () => {
+		if(search === '') {
+			return todos;
+		} else {
+			return todos.filter(todo => todo.content.toLowerCase().includes(search.toLowerCase()));
+		}
+	}
+
+	const filteredTodos = getFilteredData();
+	// const getAnalyzedData = () => {
+	// 	const totalCount = todos.length;
+	// 	const doneCount = todos.filter(todo => todo.isDone).length;
+	// 	const notDoneCount = totalCount - doneCount;
+	//
+	// 	return {totalCount, doneCount, notDoneCount};
+	// }
+
+	// 의존성배열 : deps
+	// 의존성배열이 변화해야지만 반환함
+	const {totalCount, doneCount, notDoneCount} = useMemo(() => {
+		console.log('memo');
+		const totalCount = todos.length;
+		const doneCount = todos.filter(todo => todo.isDone).length;
+		const notDoneCount = totalCount - doneCount;
+
+		return {totalCount, doneCount, notDoneCount};
+	}, [todos]);
+
+	// const {totalCount, doneCount, notDoneCount} = getAnalyzedData();
+
+	return (
+		<div className={"List"}>
+			<h4>Todo List 🍾</h4>
+			<div>
+				<div>total: {totalCount}</div>
+				<div>done: {doneCount}</div>
+				<div>notDone: {notDoneCount}</div>
+			</div>
+			<input
+				placeholder={"검색어를 입력해주세요"}
+				value={search}
+				onChange={onChange}/>
+			<div className={"todos_wrapper"}>
+				{
+					filteredTodos.map(todo => {
+						return <TodoItem
+							key={todo.id}
+							{...todo}
+							onUpdate={onUpdate}
+							onDelete={onDelete}/>;
+					})
+				}
+			</div>
+		</div>
+	)
+}
+
+export default List;
