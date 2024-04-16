33a91f25da69cee43adb26802561c185c2376401
ywyim <qndyqldy@gmail.com>
2024-04-16 23:36:48 +0900

    feat : day 20 미션 (삭제하기 버튼 및 기능 추가)

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -2,9 +2,12 @@ import "./DiaryItem.css";
 import Button from "./Button.jsx";
 import {getEmotionImage} from "../util/get-emotion-item.js";
 import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {DiaryDispatchContext} from "../App.jsx";
 
 const DiaryItem = ({id, emotionId, createdDate, content}) => {
   const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
 
   return (
     <div className={"DiaryItem"}>
@@ -28,7 +31,12 @@ const DiaryItem = ({id, emotionId, createdDate, content}) => {
       <div className={"button_section"}>
         <Button
           text={"수정하기"}
-          onClick={() => {nav(`/edit/${id}`)}}
+          onClick={() => nav(`/edit/${id}`)}
+        />
+        <Button
+          text={"삭제하기"}
+          type={"NEGATIVE"}
+          onClick={() => onDelete(id)}
         />
       </div>
     </div>
